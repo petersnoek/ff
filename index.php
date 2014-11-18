ed22ad201b2d0434435be49daee64a9fca9cda42
Peter Snoek <petersnoek@davinci.nl>
2014-11-18 11:47:26 +0100

    initial file

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+
+<head>
+	<title>Fighting Fantasy</title>
+</head>
+
+<body>
+Deze pagina gaat over "fighting fantasy"'<br>
+<?php
+	// print hallo
+	echo "hallo";
+	echo "<br>";
+	
+	/* print the date */
+	date_default_timezone_set('UTC');
+	echo date("d-m-Y");
+	echo "<br>";
+	
+	// print 1 to 10 met een for-lus
+	echo "FOR-LUS<br>";
+	for ($i = 1; $i <= 10; $i++) {
+		echo $i . "<br>";
+	}
+	
+	// print 1 to 10 met een while lus
+	echo "WHILE-LUS<br>";
+	$i = 1;
+	while ($i <= 10) {
+		echo $i . "<br>";
+		$i++;  
+	}
+	
+?>
+
+</body>
+</html>
